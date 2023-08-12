4d16190b803762a8150f46017c04fcffe44d15ad
SP4UBW <76266805+SP4UBW@users.noreply.github.com>
2023-08-12 12:17:09 +0200

    Update rssi_sbar.hpp

--- libs/views/rssi_sbar.hpp
+++ libs/views/rssi_sbar.hpp
@@ -228,10 +228,21 @@ else
     //Zamiana na µV
     //s16Number = (1 << ((s16Number + 127 + 64) / 6));
     //s16Number = (1 << ((s16Number + 127 + 64) >> 2)) >> 1;        
-    Display.PrintFixedDigitsNumber2(((1 << ((s16Number + 127 + 64) >> 2)) >> 1), 0, 5);
+    //Display.PrintFixedDigitsNumber2(((1 << ((s16Number + 127 + 64) >> 2)) >> 1), 0, 5);
          
        // Display.Print((1 << ((s16Number + 127 + 64) >> 2)) >> 1);
-     
+
+   if (s16Number<-129) {Display.PrintFixedDigitsNumber2(0, 0, 3);}
+     else if (s16Number<-67)  {Display.PrintFixedDigitsNumber2(500, 0, 3);}
+     else if (s16Number<-73)  {Display.PrintFixedDigitsNumber2(250, 0, 3);}
+     else if (s16Number<-79)  {Display.PrintFixedDigitsNumber2(126, 0, 3);}
+     else if (s16Number<-85)  {Display.PrintFixedDigitsNumber2(63, 0, 3);}
+     else if (s16Number<-91)  {Display.PrintFixedDigitsNumber2(32, 0, 3);}
+     else if (s16Number<-97)  {Display.PrintFixedDigitsNumber2(16, 0, 3);}
+     else if (s16Number<-103) {Display.PrintFixedDigitsNumber2(8, 0, 3);}
+     else if (s16Number<-109) {Display.PrintFixedDigitsNumber2(4, 0, 3);}
+     else if (s16Number<-115) {Display.PrintFixedDigitsNumber2(2, 0, 3);}        
+     else if (s16Number<-121) {Display.PrintFixedDigitsNumber2(1, 0, 3);}        
          
       }   
    }
