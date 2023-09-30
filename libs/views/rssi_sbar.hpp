9040c7e141ca4390e130374b57d9c92e18241587
SP4UBW <76266805+SP4UBW@users.noreply.github.com>
2023-09-30 03:36:06 +0200

    Update rssi_sbar.hpp

--- libs/views/rssi_sbar.hpp
+++ libs/views/rssi_sbar.hpp
@@ -175,7 +175,7 @@ void ProcessDrawings()
   
          // memcpy(pDData + 3 + 5*0 + 0, gSmallLeters + 128 * 1 + 96, 5);  //Litera A
          }
-        if (!gDisplayBuffer[128 * 4 + 14] && gDisplayBuffer[128 * 4 + 16])
+        if (gDisplayBuffer[128 * 4 + 16])
          {
           memset(gDisplayBuffer + 128 * 6, 0, 22);
           memset(pDData - 384, 0, 512);
@@ -187,7 +187,7 @@ void ProcessDrawings()
          }
        //memset(gDisplayBuffer + 128 * 2, 0, 22);
        //memset(gDisplayBuffer + 128 * 6, 0, 22);
-         
+       GPIOB->DATA |= GPIO_PIN_6; wlaczenie podswietlenia  
        PrintNumber(RssiData.s16Rssi);
        PrintSValue(RssiData.u8SValue);
        PrintSbar(RssiData.u8SValue);
